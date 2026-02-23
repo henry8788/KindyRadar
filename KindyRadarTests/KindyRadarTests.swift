60f12fe574407978d0d799fa995f6ead07e1270d
Henry Lin <cootie8788@gmail.com>
2026-02-23 21:52:38 +0800

    Initial Commit

---
+++ KindyRadarTests/KindyRadarTests.swift
@@ -0,0 +1,16 @@
+//
+//  KindyRadarTests.swift
+//  KindyRadarTests
+//
+//  Created by Henry Lin on 2026/2/23.
+//
+
+import Testing
+
+struct KindyRadarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
